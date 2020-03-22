ab5ac104aa7d162dc980667e966a9d13a7b7cd56
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2020-03-22 11:45:21 -0400

    level loader + prefab created

---
+++ Planetoids - game/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    //Pass crossfade in levelloader object to transition
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    void Update()
+    {
+        
+    }
+
+    //Call for level transition
+
+    public void loadTargetLevel(int targetLevelIndex)
+    {
+        StartCoroutine(LoadLevel(targetLevelIndex));
+    }
+
+    //Will be called by loading functions
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
--- Planetoids - game/Assets/Scripts/TransitionController.cs
+++ Planetoids - game/Assets/Scripts/TransitionController.cs
@@ -6,35 +6,21 @@ using UnityEngine.SceneManagement;
 public class TransitionController : MonoBehaviour
 {
 
-    private bool collisionControl;
     public GameObject victory;
 
     void Start()
     {
-        collisionControl = true;
-    }
 
-    void Update()
-    {
-        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collisionControl)
-        {
-            victory.SetActive(true);
-            Invoke("load", 2.0f);
-
-            //collisionControl = false;
-            
-            //FindObjectOfType<LevelChanger>().FadeToLevel();
-        }
-        
+        victory.SetActive(true);
+        Invoke("load", 2.0f);
     }
 
     private void load()
     {
-        SceneManager.LoadScene(0);
+        FindObjectOfType<LevelLoader>().loadTargetLevel(0);
     }
 }
