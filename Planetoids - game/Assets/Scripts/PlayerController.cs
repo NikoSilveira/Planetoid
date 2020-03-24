3c8754e66e15b378d3e58f04e7f3c2ed95dcbbb2
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2020-03-23 23:24:07 -0400

    textures

--- Planetoids - game/Assets/Scripts/PlayerController.cs
+++ Planetoids - game/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         //Adjust value to modify player speed
-        moveSpeed = 1.75f;
+        moveSpeed = 1.5f;
     }
 
     void Update()
